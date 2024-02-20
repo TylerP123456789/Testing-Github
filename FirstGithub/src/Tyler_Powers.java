48ccf89f91952548b28ae1531af429357ffa7d60
126jpreiser <113187426+126jpreiser@users.noreply.github.com>
2024-02-20 12:21:00 -0500

    Update Tyler_Powers.java

--- FirstGithub/src/Tyler_Powers.java
+++ FirstGithub/src/Tyler_Powers.java
@@ -1,6 +1,8 @@
 public class Tyler_Powers {
     public static void main(String[] args) {
         System.out.println("Hello Tyler Powers");
+        for(int i = 0; i < 16; i++)
+            System.out.println("Im " + i + " years old.");
     }
 
 
