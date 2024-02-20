5497e4b6370d0e8a7a50627d9fc4e950544a78bf
TylerP123456789 <113116228+TylerP123456789@users.noreply.github.com>
2024-02-20 12:45:37 -0500

    Revert "Update Tyler_Powers.java"
    This reverts commit 48ccf89f91952548b28ae1531af429357ffa7d60.

--- FirstGithub/src/Tyler_Powers.java
+++ FirstGithub/src/Tyler_Powers.java
@@ -1,8 +1,6 @@
 public class Tyler_Powers {
     public static void main(String[] args) {
         System.out.println("Hello Tyler Powers");
-        for(int i = 0; i < 16; i++)
-            System.out.println("Im " + i + " years old.");
     }
 
 
